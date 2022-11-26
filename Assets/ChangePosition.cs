8ca511f31ad05509e7c67e4f0746dbab5c542812
Saksham Khatod <saksham.khatod92@gmail.com>
2022-11-26 07:09:26 -0600

    cleanup #1

--- Assets/ChangePosition.cs
+++ Assets/ChangePosition.cs
@@ -18,25 +18,26 @@ public class ChangePosition : MonoBehaviour
     void Update()
     {
         if(isTarget1.activeSelf == false) {
-            transform.SetPositionAndRotation(new Vector3(-26.5f, 17.4f, 33.5f), new Quaternion(0,0,0,1));
+            transform.SetPositionAndRotation(isTarget1.transform.position, isTarget1.transform.rotation);
+            // transform.SetPositionAndRotation(new Vector3(-26.5f, 17.4f, 33.5f), new Quaternion(0,0,0,1));
             isTarget1.SetActive(true);
         }
         else if(isTarget2.activeSelf == false) {
-            // UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-7.690000057220459,"y":13.90999984741211,"z":3.0899999141693117},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":3.0,"y":-0.03999999910593033,"z":3.0}}
+            transform.SetPositionAndRotation(isTarget2.transform.position, isTarget2.transform.rotation);
             // transform.position = new Vector3(-7.6f, 19.18f, 0.08f);
-            transform.SetPositionAndRotation(new Vector3(-7.6f, 19.18f, 0.08f), new Quaternion(0,0,0,1));
+            // transform.SetPositionAndRotation(new Vector3(-7.6f, 19.18f, 0.08f), new Quaternion(0,0,0,1));
             isTarget2.SetActive(true);
         }
         else if(isTarget3.activeSelf == false) {
-            // UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-60.900001525878909,"y":1.2999999523162842,"z":-0.5},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}}
+            transform.SetPositionAndRotation(isTarget3.transform.position, isTarget3.transform.rotation);
             // transform.position = new Vector3(-60.9f, 1.4f, -0.5f);
-            transform.SetPositionAndRotation(new Vector3(-60.9f, 1.4f, -0.5f), new Quaternion(0,0,0,1));
+            // transform.SetPositionAndRotation(new Vector3(-60.9f, 1.4f, -0.5f), new Quaternion(0,0,0,1));
             isTarget3.SetActive(true);
         }
         else if(isTargetHome.activeSelf == false) {
-            // UnityEditor.TransformWorldPlacementJSON:{"position":{"x":-7.429999828338623,"y":-0.9574999809265137,"z":4.420000076293945},"rotation":{"x":0.0,"y":0.0,"z":0.0,"w":1.0},"scale":{"x":1.0,"y":1.0,"z":1.0}}
+            transform.SetPositionAndRotation(isTargetHome.transform.position, isTargetHome.transform.rotation);
             // transform.position = new Vector3(-7.42f, -0.957f, 4.42f);
-            transform.SetPositionAndRotation(new Vector3(-7.42f, -0.957f, 4.42f), new Quaternion(0,0,0,1));
+            // transform.SetPositionAndRotation(new Vector3(-7.42f, -0.957f, 4.42f), new Quaternion(0,0,0,1));
             isTargetHome.SetActive(true);
         }
     }
